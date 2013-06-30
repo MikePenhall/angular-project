62dbc2600fb360c902ed0fdbb1ea30ed95106c9f
James McCann <jmccnz@gmail.com>
2013-07-01 11:54:55 +1200

    Fixed submissions index listing

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,8 +1,8 @@
 var AngularForm = angular.module('AngularForm', ['templates'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-        when('/index', {templateUrl: 'submissions-list.html'}).
-        when('/step_one', {templateUrl: 'form1.html'}).
-        when('/step_two', {templateUrl: 'form2.html'}).
+        when('/index', {templateUrl: 'submissions.html', controller: 'SubmissionsCtrl'}).
+        when('/step_one', {templateUrl: 'form1.html', controller: 'SubmissionsCtrl'}).
+        when('/step_two', {templateUrl: 'form2.html', controller: 'SubmissionsCtrl'}).
         otherwise({redirectTo: '/index'});
 }]);
