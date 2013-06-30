88449eaf744e69819c5f33dd53174962e1344fba
James McCann <jmccnz@gmail.com>
2013-07-01 11:43:17 +1200

    Added ng-view direction, modified index template

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,7 +1,8 @@
 var AngularForm = angular.module('AngularForm', ['templates'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.
+        when('/index', {templateUrl: 'index.html'}).
         when('/step_one', {templateUrl: 'form1.html'}).
         when('/step_two', {templateUrl: 'form2.html'}).
-        otherwise({redirectTo: '/step_one'});
+        otherwise({redirectTo: '/index'});
 }]);
