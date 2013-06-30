b64516710b203e55e819ce7c579f757826918b29
MikePenhall <mikepenhall@gmail.com>
2013-07-01 11:48:41 +1200

    changed submissions list template name in app.js

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,7 +1,7 @@
 var AngularForm = angular.module('AngularForm', ['templates'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-        when('/index', {templateUrl: 'index.html'}).
+        when('/index', {templateUrl: 'submissions-list.html'}).
         when('/step_one', {templateUrl: 'form1.html'}).
         when('/step_two', {templateUrl: 'form2.html'}).
         otherwise({redirectTo: '/index'});
